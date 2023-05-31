c8cea6717ee71273b356cc98e287d729e17fe329
Francois Zhao <francois.zhao.fz@gmail.com>
2023-05-31 16:23:31 +0200

    [ADD] mongo DB database

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,27 @@
 import express from "express";
 import data from "./data.js";
 import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import seedRouter from "./routes/seedRoutes.js";
+import productRouter from "./routes/productRoutes.js";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
 
 const app = express();
-
 app.use(cors());
 
-app.get("/api/products", (req, res) => {
-  console.log("Sending Products Data");
-  res.send(data.products);
-});
-
-app.get("/api/products/slug/:slug", (req, res) => {
-  console.log("Sending Slug Data");
-  const product = data.products.find((x) => x.slug === req.params.slug);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: "Product Not Found" });
-  }
-});
+app.use("/api/seed", seedRouter);
+app.use("/api/products", productRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
